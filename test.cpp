2320f0ed6125c877a5a7422198676c99df69d728
MONTH4 <goei4559@gmail.com>
2024-05-27 16:22:57 +0900

    MFMF-2 <파이그래프만들기>

--- test.cpp
+++ test.cpp
@@ -4,5 +4,6 @@ int main(){
     printf("test for jira");
     printf("test for push!"); // 이슈키 이용한 푸쉬 시도2
     printf("test for push!"); // 이슈키 이용한 푸쉬 시도3
+    printf("make new branch!"); // 지라를 통한 브렌치 생성 및 푸쉬
     return 0;
 }
